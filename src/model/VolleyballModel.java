15d3894a69c307d83c6c8662a695f19e362f33a8
Angela Rivas <snowcats001@gmail.com>
2018-09-20 13:05:52 -0600

    Added import statements, set & get variables in model, and created one output statement in GUI in start() class.

---
+++ src/model/VolleyballModel.java
@@ -0,0 +1,55 @@
+package model;
+
+public class VolleyballModel
+{
+
+	private int scoreBoard;
+	private boolean inBounds;
+	private int addPoint;
+	private String playerName;
+	
+	public VolleyballModel()
+	{
+		//Default constructor 
+	}
+	
+	public VolleyballModel (int scoreBoard, boolean inBounds, int addPoint, String playerName)
+	{
+		this.scoreBoard = scoreBoard;
+		this.inBounds = inBounds;
+		this.addPoint = addPoint;
+		this.playerName = playerName;
+	}
+	public int getScoreBoard()
+	{
+		return scoreBoard;
+	}
+	public boolean getInBounds()
+	{
+		return inBounds;
+	}
+	public int getAddPoint()
+	{
+		return addPoint;
+	}
+	public String getPlayerName()
+	{
+		return playerName;
+	}
+	public void setScoreBoard(int scoreBoard)
+	{
+		this.scoreBoard = scoreBoard;
+	}
+	public void setInBounds(boolean inBounds)
+	{
+		this.inBounds = inBounds;
+	}
+	public void setAddPoint(int addPoint)
+	{
+		this.addPoint = addPoint;
+	}
+	public void setPlayerName(String playerName)
+	{
+		this.playerName = playerName;
+	}
+}
--- src/volleyball/Controller.java
+++ src/volleyball/Controller.java
@@ -1,9 +1,63 @@
 package volleyball;
 
+/**
+ * Import Section
+ * @param Import Section 
+ */
+import java.util.Scanner;
+import model.VolleyballModel;
+import javax.swing.JOptionPane;
+
 public class Controller
 {
+	private VolleyballModel volleyballGame;
+	
+	private Scanner inputScanner;
+	/**
+	 * Defines variables 
+	 * @param defines variables publicly
+	 */
+	public Controller()
+	{
+		volleyballGame = new VolleyballModel();
+		inputScanner = new Scanner(System.in);
+	}
+	
 	public void start()
 	{
+		String userInput = JOptionPane.showInputDialog(null, "How many points on your scoreboard?");
+		int scoreBoard = 0;
+		if(validInt(userInput))
+		{
+			scoreBoard = Integer.parseInt(userInput);
+		}
+		
+		volleyballGame.setScoreBoard(scoreBoard);
+		if(scoreBoard > 15)
+		{
+			JOptionPane.showMessageDialog(null, "your scoreboard has " + volleyballGame.getScoreBoard() + " points, pick up your game!");
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(null, "your scoreboard has " + volleyballGame.getScoreBoard() + " points, you're almost there!");
+		}
 		
 	}
+	public boolean validInt(String maybeInt)
+	{
+		boolean isValid = false;
+		
+		try
+		{
+			Integer.parseInt(maybeInt);
+			isValid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "You'll need to type in a whole number :)");
+		}
+		return isValid;
+		
+	}
+
 }
